a91ea426fd119a6d0e5c06988e684f2aed642846
mwmichalek <mwmichalek@gmail.com>
2021-09-29 21:45:24 -0400

    Starting to generalize.

---
+++ src/Mwm.Asanate.Client.Blazor/Store/Features/Shared/Actions/LoadActions.cs
@@ -0,0 +1,18 @@
+﻿using Mwm.Asanate.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mwm.Asanate.Client.Blazor.Store.Features.Shared.Actions {
+    public class LoadAction<TEntity> where TEntity : NamedEntity {
+    }
+
+    public class LoadSuccessAction<TEntity> where TEntity : NamedEntity {
+
+    }
+
+    public class LoadFailureAction<TEntity> where TEntity : NamedEntity {
+
+    }
+}
