2d53d3d22056e6dce5e4c157115ce13d80579e5a
mwmichalek <mwmichalek@gmail.com>
2021-10-01 20:49:58 -0400

    We have stuff working!

---
+++ src/Mwm.Asanate.Client.Blazor/Models/Tsks/TskModel.cs
@@ -0,0 +1,36 @@
+﻿using Mwm.Asanate.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mwm.Asanate.Client.Blazor.Models.Tsks {
+    public class TskModel {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ExternalId { get; set; }
+
+        public Status Status { get; set; }
+
+        public bool IsArchived { get; set; }
+
+        public string Notes { get; set; }
+
+        public DateTime? CompletedDate { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public DateTime? StartedDate { get; set; }
+
+        public string InitiativeName { get; set; }
+
+        public string ProjectName { get; set; }
+
+        public string CompanyName { get; set; }
+    }
+}
--- src/Mwm.Asanate.Client.Blazor/Pages/Today.razor.cs
+++ src/Mwm.Asanate.Client.Blazor/Pages/Today.razor.cs
@@ -1,8 +1,11 @@
 ﻿using Fluxor;
 using Fluxor.Blazor.Web.Components;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using Mwm.Asanate.Client.Blazor.Models.Tsks;
 using Mwm.Asanate.Client.Blazor.Services;
 using Mwm.Asanate.Client.Blazor.Services.State;
+using Mwm.Asanate.Client.Blazor.Store.Features.Shared.Actions;
 using Mwm.Asanate.Client.Blazor.Store.State.Shared;
 using Mwm.Asanate.Data;
 using Mwm.Asanate.Domain;
@@ -15,34 +18,56 @@ namespace Mwm.Asanate.Client.Blazor.Pages {
     public partial class Today : FluxorComponent {
 
         [Inject]
+        ILogger<Today> Logger { get; set; }
+
+        [Inject]
         public IState<EntityState<Tsk>> TsksState { get; set; }
 
         [Inject]
-        public IState<EntityState<Initiative>> InitiativeState { get; set; }
+        public IState<EntityState<Initiative>> InitiativesState { get; set; }
 
         [Inject]
-        public IState<EntityState<Company>> CompanyState { get; set; }
+        public IState<EntityState<Company>> CompaniesState { get; set; }
 
         [Inject]
-        public IState<EntityState<Project>> ProjectState { get; set; }
+        public IState<EntityState<Project>> ProjectsState { get; set; }
 
         [Inject]
         public EntityStateService EntityService { get; set; }
 
+        public List<TskModel> TskModels => BuildTskModels();
 
+        [Inject]
+        public IActionSubscriber ActionSubscriber { get; set; }
 
         protected override void OnInitialized() {
             if (TsksState.Value.Entities is null) 
                 EntityService.Load<Tsk>();
-            if (InitiativeState.Value.Entities is null)
+            if (InitiativesState.Value.Entities is null)
                 EntityService.Load<Initiative>();
-            if (CompanyState.Value.Entities is null)
+            if (CompaniesState.Value.Entities is null)
                 EntityService.Load<Company>();
-            if (ProjectState.Value.Entities is null)
+            if (ProjectsState.Value.Entities is null)
                 EntityService.Load<Project>();
 
-
+            //ActionSubscriber.SubscribeToAction<LoadSuccessAction<Tsk>>(this, (action) => LoadModels());
             base.OnInitialized();
+        }
+
+        private List<TskModel> BuildTskModels() {
+            return TsksState.Value.Entities.Select(t => {
+                Initiative initiative = InitiativesState.Value.Entities.SingleOrDefault(i => i.Id == t.InitiativeId);
+                Project project = (initiative != null) ? ProjectsState.Value.Entities.SingleOrDefault(p => p.Id == initiative.ProjectId) : null;
+                Company company = (project != null) ? CompaniesState.Value.Entities.SingleOrDefault(c => c.Id == project.CompanyId) : null;
+
+                return new TskModel {
+                    Id = t.Id,
+                    Name = t.Name,
+                    InitiativeName = initiative?.Name,
+                    ProjectName = project?.Name,
+                    CompanyName = company?.Name
+                };
+            }).ToList();
         }
     }
 }
