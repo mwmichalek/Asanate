8bb8616fea6a339c272a15443b196b001df3fd87
Michalek <mwmichalek@gmail.com>
2021-10-06 10:23:36 -0400

    Removed Status code after simplification

--- src/Mwm.Asanate.Domain/Status.cs
+++ src/Mwm.Asanate.Domain/Status.cs
@@ -18,26 +18,6 @@ namespace Mwm.Asanate.Domain {
 
     public static class StatusExtensions {
 
-        //private static Dictionary<string, Status> lookup = new Dictionary<string, Status> {
-        //    { "Open", Status.Open },
-        //    { "Queued", Status.Open },
-        //    { "Planned", Status.Planned },
-        //    { "Ready To Start", Status.ReadyToStart },
-        //    { "In Progress", Status.InProgress },
-        //    { "Pending", Status.Pending },
-        //    { "Done", Status.Done },
-        //};
-
-        //private static Dictionary<string, Status> lookup = new Dictionary<string, Status> {
-        //    { Status.Open, "Open" },
-
-        //    { "Planned", Status.Planned },
-        //    { "Ready To Start", Status.ReadyToStart },
-        //    { "In Progress", Status.InProgress },
-        //    { "Pending", Status.Pending },
-        //    { "Done", Status.Done },
-        //};
-
         public static Status ToStatus(this string statusStr) {
             if (Enum.TryParse(typeof(Status), statusStr.Replace(" ", "_"), true, out object? statusObj))
                 return (Status)statusObj;
@@ -49,41 +29,4 @@ namespace Mwm.Asanate.Domain {
 
     }
 
-    //public class Status : NamedEntity {
-
-    //    public Status(string value) {
-    //        Value = value;
-    //    }
-
-    //    public string Value { get; private set; }
-
-    //    public static Status Open => new Status("Open");
-    //    public static Status Planned => new Status("Planned");
-
-    //    // Chose not to include this.  Perhaps we should just make sure to many 
-    //    // items reach the Planned state.  Leave them Open, the dumping ground.
-    //    //public static Status Queued => new Status("Queued");
-
-    //    public static Status ReadyToStart => new Status("Ready To Start");
-    //    public static Status InProgress => new Status("In Progress");
-    //    public static Status Pending => new Status("Pending");
-    //    public static Status Done => new Status("Done");
-
-    //    public static List<Status> Statuses = new List<Status> {
-    //        Open,
-    //        Planned,
-    //        //Queued,
-    //        ReadyToStart,
-    //        InProgress,
-    //        Pending,
-    //        Done
-    //    };
-    //}
-
-    //public static class StatusExtensions {
-
-    //    public static Status ToStatus(this string statusName) {
-    //        return Status.Statuses.SingleOrDefault(s => s.Value == statusName);
-    //    }
-    //}
 }
