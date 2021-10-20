bc69de0e75d305cc29edf8e2367b4c2dbc733442
mwmichalek <mwmichalek@gmail.com>
2021-10-20 18:43:40 -0400

    Created Popup component, just need to add a popup to the component.

---
+++ src/Mwm.Asanate.Client.Blazor/Components/TskPopup.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using Mwm.Asanate.Client.Blazor.Models.Tsks;
+using System.Threading.Tasks;
+
+namespace Mwm.Asanate.Client.Blazor.Components {
+    public partial class TskPopup : ComponentBase {
+
+        [Inject]
+        public ILogger<TskPopup> Logger { get; set; }
+
+        [Parameter]
+        public TskModel TskModel { get; set; }
+
+        protected override async Task OnInitializedAsync() {
+            Logger.LogInformation($"TskPop!!!!!");
+            
+            //await
+                
+        }
+    }
+}
