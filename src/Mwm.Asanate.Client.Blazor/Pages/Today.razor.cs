99acb617bdef2a387344682d3a4619c1cd8f8190
Michalek <mwmichalek@gmail.com>
2021-11-04 12:53:19 -0400

    Added some additional pages.

--- src/Mwm.Asanate.Client.Blazor/Pages/Today.razor.cs
+++ src/Mwm.Asanate.Client.Blazor/Pages/Today.razor.cs
@@ -21,10 +21,6 @@ using Mwm.Asanate.Client.Blazor.Components;
 namespace Mwm.Asanate.Client.Blazor.Pages {
     public partial class Today : FluxorComponent {
 
-        public SfKanban<TskModel> KanbanBoard;
-
-        public TskPopup TskPopup;
-
         [Inject]
         ILogger<Today> Logger { get; set; }
 
@@ -43,111 +39,6 @@ namespace Mwm.Asanate.Client.Blazor.Pages {
         [Inject]
         public EntityStateFacade EntityStateFacade { get; set; }
 
-        //private bool rebuildTskModels = false;
-        private List<TskModel> tskModels = new List<TskModel>();
-
-        public IEnumerable<TskModel> TskModels {
-            get => tskModels;
-            set { }
-        }
-
-        public List<string> Companies => TskModels.Select(t => t.CompanyName).Distinct().ToList();
-
-        public List<string> StatusNames => TskModels.Select(t => t.Status)
-                                                    .Distinct()
-                                                    .OrderBy(s => (int)s)
-                                                    .Select(s => s.ToStr())
-                                                    .ToList();
-
-        [Inject]
-        public IActionSubscriber ActionSubscriber { get; set; }
-
-        public bool IsLoading() => TsksState.IsLoading() ||
-                                   InitiativesState.IsLoading() ||
-                                   ProjectsState.IsLoading() ||
-                                   CompaniesState.IsLoading();
-
-        public bool HasErrors() => TsksState.HasErrors() ||
-                                   InitiativesState.HasErrors() ||
-                                   ProjectsState.HasErrors() ||
-                                   CompaniesState.HasErrors();
-
-        public bool HasValues() => TsksState.HasValue(true) &&
-                                   InitiativesState.HasValue(true) &&
-                                   ProjectsState.HasValue(true) &&
-                                   CompaniesState.HasValue(true);
-
-        protected override void OnInitialized() {
-            if (!TsksState.HasValue())
-                EntityStateFacade.Load<Tsk>();
-            if (!InitiativesState.HasValue())
-                EntityStateFacade.Load<Initiative>();
-            if (!CompaniesState.HasValue())
-                EntityStateFacade.Load<Company>();
-            if (!ProjectsState.HasValue())
-                EntityStateFacade.Load<Project>();
-
-            TsksState.StateChanged += (s, e) => BuildTskModels();
-            InitiativesState.StateChanged += (s, e) => BuildTskModels();
-            ProjectsState.StateChanged += (s, e) => BuildTskModels();
-            CompaniesState.StateChanged += (s, e) => BuildTskModels();
-
-            base.OnInitialized();
-        }
-
-
-        private void BuildTskModels(string triggerBy = null) {
-            if (HasValues()) {
-                tskModels = TsksState.Value.Entities?.Where(t => !t.IsArchived).Select(t => {
-                    Initiative initiative = InitiativesState.FindById(t.InitiativeId);
-                    Project project = (initiative != null) ? ProjectsState.FindById(initiative.ProjectId) : null;
-                    Company company = (project != null) ? CompaniesState.FindById(project.CompanyId) : null;
-
-                    return new TskModel {
-                        Id = t.Id,
-                        Name = t.Name,
-                        ExternalId = t.ExternalId,
-                        Status = t.Status,
-                        DurationEstimate = t.DurationEstimate,
-                        DurationCompleted = t.DurationCompleted,
-                        Notes = t.Notes,    
-                        DueDate = t.DueDate,
-                        StartDate = t.StartDate,
-                        StartedDate = t.StartedDate,
-                        CompletedDate = t.CompletedDate,
-                        InitiativeName = initiative?.Name,
-                        ProjectName = project?.Name,
-                        CompanyName = company?.Name
-                    };
-                }).ToList();
-                Logger.LogInformation($"Built {tskModels.Count} TskModels");
-            }
-        }
-
-        public void DragStopHandler(DragEventArgs<TskModel> args) {
-            foreach (var tskModel in args.Data) {
-                try {
-                    var tsk = TsksState.FindById(tskModel.Id);
-
-                    Logger.LogInformation($"Moved: {tskModel.Name}, FromStatus: {tsk.Status} ToStatus: {tskModel.Status}");
-                    EntityStateFacade.Update<Tsk, TskUpdate.Command>(new TskUpdate.Command {
-                        Id = tskModel.Id,
-                        Name = tsk.Name,
-                        Status = tskModel.Status
-                    });
-                } catch (Exception ex) {
-                    Logger.LogError($"Unable to update: {tskModel.Name}, {ex}");
-                }
-            }
-        }
-
-        public void DialogOpenHandler(DialogOpenEventArgs<TskModel> args) {
-            args.Cancel = true;
-            Logger.LogInformation("DialogOpenHandler!!!!!");
-            TskPopup.Update(args.Data);
-        }
-
-        public TskModel SelectedTskModel { get; set; }
 
     }
 }
