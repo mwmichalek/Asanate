635c0d089ec0e02f23430054b4db2fb886f00c4c
Michalek <mwmichalek@gmail.com>
2021-11-04 14:07:55 -0400

    Added Project Add, untested.

---
+++ src/Mwm.Asanate.Application/Projects/Commands/ProjectAdd.Handler.cs
@@ -0,0 +1,58 @@
+﻿using MediatR;
+using System;
+using FluentResults;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mwm.Asanate.Application.Shared.Commands;
+using Mwm.Asanate.Domain;
+using Microsoft.Extensions.Logging;
+using Mwm.Asanate.Application.Interfaces.Persistance;
+using Mwm.Asanate.Application.Utils;
+using System.Threading;
+
+namespace Mwm.Asanate.Application.Projects.Commands {
+    public partial class ProjectAdd {
+
+        public class Handler : IRequestHandler<Command, Result<int>> {
+
+            private ILogger<Handler> _logger;
+
+            private readonly IRepository<Project> _projectRepository;
+
+            public Handler(ILogger<Handler> logger,
+                           IRepository<Project> projectRepository) {
+                _logger = logger;
+                _projectRepository = projectRepository;
+            }
+
+            public async Task<Result<int>> Handle(Command command, CancellationToken cancellationToken) {
+                return await CreateProject(command);
+            }
+
+            private async Task<Result<int>> CreateProject(Command command) {
+                if (string.IsNullOrEmpty(command.Name))
+                    return Result.Fail("Project Name can't be null.");
+
+                try {
+                    var project = new Project {
+                        Name = command.Name
+                    };
+                    _projectRepository.Add(project);
+                    await _projectRepository.SaveAsync();
+                    _logger.LogInformation($"Project Added: {project.Name}");
+                    return Result.Ok(project.Id).WithSuccess(project.ToSuccess(ResultAction.Add));
+                       
+                } catch (Exception ex) {
+                    _logger.LogError($"Project Add Failure: {ex}");
+                    return Result.Fail(new Error("Unable to create Project").CausedBy(ex));
+                }
+            }
+        }
+    }
+}
+
+
+
+
---
+++ test/Mwm.Asanate.Service.Tests/Mediator/Commands/InitiativeUpdateTests.cs
@@ -0,0 +1,52 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Mwm.Asanate.Application.Initiatives.Commands;
+using Mwm.Asanate.Application.Tsks.Commands;
+using Mwm.Asanate.Application.Utils;
+using Mwm.Asanate.Data;
+using Mwm.Asanate.Domain;
+using Mwm.Asanate.Persistance.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Mwm.Asanate.Service.Tests.Mediator.Commands {
+
+    [Collection("Generic")]
+    public class InitiativeUpdateTests {
+        private readonly IMediator _mediator;
+        private readonly IDatabaseContext _databaseContext;
+        private readonly ITestOutputHelper _output;
+
+        public InitiativeUpdateTests(IMediator mediator, IDatabaseContext databaseContext, ITestOutputHelper output) {
+            _databaseContext = databaseContext;
+            _mediator = mediator;
+            _output = output;
+
+        }
+
+        [Fact]
+        public async Task UpdateSimpleInitiative() {
+            var name = $"SimpleInitiative_{DateTime.Now}";
+            var command = new InitiativeUpdate.Command {
+                Id = 1,
+                Name = name
+            };
+
+            var result = await _mediator.Send(command);
+            Assert.True(result.IsSuccess, $"UpdateSimpleInitiative failed: {result}");
+            Assert.True(result.HasSuccess<EntitySuccess<Initiative>>(t => t.Action == ResultAction.Update));
+            Assert.True(result.HasSuccess<EntitySuccess<Initiative>>(t => t.Entity.Name == command.Name));
+
+            var initiative = _databaseContext.Initiatives.Find(result.Value);
+            Assert.Equal(command.Name, initiative.Name);
+
+            _output.WriteLine(result.ToString());
+        }
+
+    }
+}
