9a8f17cdcca429dfd63beb3de35e8e0966ac4b7b
mwmichalek <mwmichalek@gmail.com>
2021-11-04 21:22:41 -0400

    Making progress on Notes.

---
+++ src/Mwm.Asanate.Client.Blazor/Models/Shared/DropDownEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Mwm.Asanate.Client.Blazor.Models.Shared {
+    public class DropDownEntity {
+
+        public int Id {  get; set; }
+        public string Name { get; set; }
+    }
+}
--- src/Mwm.Asanate.Client.Blazor/Pages/Notes.razor.cs
+++ src/Mwm.Asanate.Client.Blazor/Pages/Notes.razor.cs
@@ -9,6 +9,10 @@ using Mwm.Asanate.Domain;
 using Mwm.Asanate.Client.Service.Store.State.Shared;
 using Fluxor;
 using Microsoft.AspNetCore.Components.Web;
+using Mwm.Asanate.Client.Service.Store.Features.Shared.Helpers;
+using System.Collections.Generic;
+using Mwm.Asanate.Client.Blazor.Models.Shared;
+using System.Linq;
 
 namespace Mwm.Asanate.Client.Blazor.Pages {
     public partial class Notes : FluxorComponent {
@@ -20,9 +24,68 @@ namespace Mwm.Asanate.Client.Blazor.Pages {
         public IState<EntityState<Tsk>> TsksState { get; set; }
 
         [Inject]
+        public IState<EntityState<Initiative>> InitiativesState { get; set; }
+
+        [Inject]
+        public IState<EntityState<Project>> ProjectsState { get; set; }
+
+        [Inject]
+        public IState<EntityState<Company>> CompaniesState { get; set; }
+
+        [Inject]
         public EntityStateFacade EntityStateFacade { get; set; }
 
         public TskModel NewTskModel { get; set; } = new TskModel();
+
+        public List<TskModel> SavedTskModels { get; set; } = new List<TskModel>();
+
+        public List<DropDownEntity> ProjectDropDownEntities { get; set; } = new List<DropDownEntity>();
+
+        private int selectedProjectId;
+        public int SelectedProjectId {
+            get { return selectedProjectId; }
+            set {
+                selectedProjectId = value;
+                UpdateInitiativeDropDown();
+            }
+        }
+
+        public List<DropDownEntity> InitiativeDropDownEntities { get; set; } = new List<DropDownEntity>();
+
+        protected override void OnInitialized() {
+            if (!TsksState.HasValue())
+                EntityStateFacade.Load<Tsk>();
+            if (!InitiativesState.HasValue())
+                EntityStateFacade.Load<Initiative>();
+            if (!CompaniesState.HasValue())
+                EntityStateFacade.Load<Company>();
+            if (!ProjectsState.HasValue())
+                EntityStateFacade.Load<Project>();
+
+            TsksState.StateChanged += (s, e) => Saved(e);
+            InitiativesState.StateChanged += (s, e) => UpdateInitiativeDropDown();
+            ProjectsState.StateChanged += (s, e) => UpdateProjectDropDown();
+
+            base.OnInitialized();
+        }
+
+        private void UpdateInitiativeDropDown() {
+            InitiativeDropDownEntities = InitiativesState.Value.Entities.Where(i => i.ProjectId == selectedProjectId)
+                                                                        .Select(i => new DropDownEntity { Id = i.Id, Name = i.Name })
+                                                                        .ToList();
+        }
+
+        private void UpdateProjectDropDown() {
+            ProjectDropDownEntities = ProjectsState.Value.Entities.Select(p => new DropDownEntity { Id = p.Id, Name = p.Name })
+                                                                  .ToList();
+        }
+
+        public void Saved(EntityState<Tsk> entityState) {
+            if (entityState.CurrentEntity.Name == NewTskModel.Name) {
+                NewTskModel = new TskModel();
+                SavedTskModels.Insert(0, NewTskModel);
+            }
+        }
 
         public void Save() {
             Logger.LogInformation($"Saving Popup.");
