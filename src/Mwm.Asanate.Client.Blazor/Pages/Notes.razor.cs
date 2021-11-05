47601f347f50c3e125602760f20b9d0c2af0de2c
Michalek <mwmichalek@gmail.com>
2021-11-05 17:01:08 -0400

    Dropdowns sorta work.

--- src/Mwm.Asanate.Client.Blazor/Pages/Notes.razor.cs
+++ src/Mwm.Asanate.Client.Blazor/Pages/Notes.razor.cs
@@ -39,9 +39,9 @@ namespace Mwm.Asanate.Client.Blazor.Pages {
 
         public List<TskModel> SavedTskModels { get; set; } = new List<TskModel>();
 
-        public List<DropDownEntity> ProjectDropDownEntities { get; set; } = new List<DropDownEntity>();
+        public IEnumerable<DropDownEntity> ProjectDropDownEntities { get; set; } = new List<DropDownEntity>();
 
-        private int selectedProjectId;
+        private int selectedProjectId = 0;
         public int SelectedProjectId {
             get { return selectedProjectId; }
             set {
@@ -50,7 +50,15 @@ namespace Mwm.Asanate.Client.Blazor.Pages {
             }
         }
 
-        public List<DropDownEntity> InitiativeDropDownEntities { get; set; } = new List<DropDownEntity>();
+        private int selectedInitiativeId = 0;
+        public int SelectedInitiativeId {
+            get { return selectedInitiativeId; }
+            set {
+                selectedInitiativeId = value;
+            }
+        }
+
+        public IEnumerable<DropDownEntity> InitiativeDropDownEntities { get; set; } = new List<DropDownEntity>();
 
         protected override void OnInitialized() {
             if (!TsksState.HasValue())
@@ -66,22 +74,49 @@ namespace Mwm.Asanate.Client.Blazor.Pages {
             InitiativesState.StateChanged += (s, e) => UpdateInitiativeDropDown();
             ProjectsState.StateChanged += (s, e) => UpdateProjectDropDown();
 
+            UpdateProjectDropDown();
+            UpdateInitiativeDropDown();
+
             base.OnInitialized();
         }
 
-        private void UpdateInitiativeDropDown() {
-            InitiativeDropDownEntities = InitiativesState.Value.Entities.Where(i => i.ProjectId == selectedProjectId)
-                                                                        .Select(i => new DropDownEntity { Id = i.Id, Name = i.Name })
-                                                                        .ToList();
-        }
+        
 
         private void UpdateProjectDropDown() {
-            ProjectDropDownEntities = ProjectsState.Value.Entities.Select(p => new DropDownEntity { Id = p.Id, Name = p.Name })
-                                                                  .ToList();
+            if (ProjectsState.HasValue()) {
+                if (selectedProjectId == 0) {
+                    var genericProject = ProjectsState.Value.Entities.SingleOrDefault(p => p.Name == "Generic");
+                    if (genericProject != null) {
+                        selectedProjectId = genericProject.Id;
+                        Logger.LogInformation($"Default Project: {genericProject.Id} {genericProject.Name}");
+                    }
+                }
+
+                ProjectDropDownEntities = ProjectsState.Value.Entities.Select(p => new DropDownEntity { Id = p.Id, Name = p.Name })
+                                                                      .ToList();
+            }
+        }
+
+        private void UpdateInitiativeDropDown() {
+            if (selectedProjectId != 0 && InitiativesState.HasValue()) {
+                if (selectedInitiativeId == 0) { 
+                    var triageInitiative = InitiativesState.Value.Entities.SingleOrDefault(i => i.ProjectId == selectedProjectId &&
+                                                                                           i.Name == "Triage");
+                    if (triageInitiative != null) {
+                        selectedInitiativeId = triageInitiative.Id;
+                        Logger.LogInformation($"Default Initiative: {triageInitiative.Id} {triageInitiative.Name}");
+                    }
+                }
+
+                InitiativeDropDownEntities = InitiativesState.Value.Entities.Where(i => i.ProjectId == selectedProjectId)
+                                                                            .Select(i => new DropDownEntity { Id = i.Id, Name = i.Name })
+                                                                            .ToList();
+            }
         }
 
         public void Saved(EntityState<Tsk> entityState) {
-            if (entityState.CurrentEntity.Name == NewTskModel.Name) {
+            if (entityState.CurrentEntity != null &&
+                entityState.CurrentEntity.Name == NewTskModel.Name) {
                 NewTskModel = new TskModel();
                 SavedTskModels.Insert(0, NewTskModel);
             }
@@ -95,7 +130,8 @@ namespace Mwm.Asanate.Client.Blazor.Pages {
 
                 EntityStateFacade.Add<Tsk, TskAdd.Command>(new TskAdd.Command {
                     Name = NewTskModel.Name,
-                    DurationEstimate = NewTskModel.DurationEstimate
+                    DurationEstimate = NewTskModel.DurationEstimate,
+                    InitiativeId = selectedInitiativeId
                 });
             } catch (Exception ex) {
                 Logger.LogError($"Unable to update: {NewTskModel.Name}, {ex}");
