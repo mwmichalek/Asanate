df225c90c24a902fec10d15ce534618e5ab04e53
Michalek <mwmichalek@gmail.com>
2021-12-12 09:38:48 -0500

    Getting closer to transition

---
+++ src/Mwm.MyQ.Client.Blayzor/Pages/Today.razor.cs
@@ -0,0 +1,44 @@
+﻿using Fluxor;
+using Fluxor.Blazor.Web.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using Mwm.MyQ.Client.Blayzor.Models.Tsks;
+using Mwm.MyQ.Client.Service.Facades;
+using Mwm.MyQ.Client.Service.Store.Features.Shared.Actions;
+using Mwm.MyQ.Client.Service.Store.Features.Shared.Helpers;
+using Mwm.MyQ.Client.Service.Store.State.Shared;
+using Mwm.MyQ.Application.Tsks.Commands;
+using Mwm.MyQ.Data;
+using Mwm.MyQ.Domain;
+using Syncfusion.Blazor.Kanban;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Syncfusion.Blazor.Popups;
+using Mwm.MyQ.Client.Blayzor.Components;
+
+namespace Mwm.MyQ.Client.Blayzor.Pages {
+    public partial class Today : FluxorComponent {
+
+        [Inject]
+        ILogger<Today> Logger { get; set; }
+
+        [Inject]
+        public IState<EntityState<Tsk>> TsksState { get; set; }
+
+        [Inject]
+        public IState<EntityState<Initiative>> InitiativesState { get; set; }
+
+        [Inject]
+        public IState<EntityState<Company>> CompaniesState { get; set; }
+
+        [Inject]
+        public IState<EntityState<Project>> ProjectsState { get; set; }
+
+        [Inject]
+        public EntityStateFacade EntityStateFacade { get; set; }
+
+
+    }
+}
--- src/Mwm.MyQ.Client.Blayzor/Program.cs
+++ src/Mwm.MyQ.Client.Blayzor/Program.cs
@@ -8,6 +8,8 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Mwm.MyQ.Common.Utils;
+using Mwm.MyQ.Client.Service.Utils;
 
 namespace Mwm.MyQ.Client.Blayzor
 {
@@ -15,13 +17,30 @@ namespace Mwm.MyQ.Client.Blayzor
     {
         public static async Task Main(string[] args)
         {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTA5OTE4QDMxMzkyZTMzMmUzMGZ4QnFHbGNEVFI4ZUcwYzZnVUJnQkFEcmxKUlFmQWl4RDc4VDdKcFRmcE09");
+
             // Add your Syncfusion license key for Blazor platform with corresponding Syncfusion NuGet version referred in project. For more information about license key see https://help.syncfusion.com/common/essential-studio/licensing/license-key.
             // Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Add your license key here"); 
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-            builder.Services.AddSyncfusionBlazor();
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //builder.Services.AddSyncfusionBlazor();
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //await builder.Build().RunAsync();
+
+            var services = builder.Services;
+            var myCloneApiUrl = builder.Configuration["MyClone:Api:Url"];
+            //var connectionString = builder.Configuration["ConnectionStrings:DatabaseContext"];
+            Console.WriteLine($"Middle Tier: [{myCloneApiUrl}]");
+            //Console.WriteLine($"Database: [{connectionString}]");
+
+            services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001") });
+
+            var configuration = services.AddConfigurationWithUserSecrets();
+            services.AddLogging();
+            services.AddClientServices();
+
+            builder.Services.AddSyncfusionBlazor();
 
             await builder.Build().RunAsync();
         }
