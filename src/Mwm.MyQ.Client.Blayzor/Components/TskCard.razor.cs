404ebf4ed7b05d53cee820aa1496be6af641b48f
mwmichalek <mwmichalek@gmail.com>
2021-12-16 14:37:35 -0500

    Progress!

---
+++ src/Mwm.MyQ.Client.Blayzor/Components/TskCard.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Mwm.MyQ.Client.Blayzor.Models.Tsks;
+
+namespace Mwm.MyQ.Client.Blayzor.Components;
+
+public partial class TskCard : ComponentBase {
+
+    [Parameter]
+    public TskModel TskModel { get; set; }
+
+    public string HourProgressDisplay =>
+             (TskModel.DurationCompleted.HasValue && TskModel.DurationEstimate.HasValue) ? $"{TskModel.DurationCompleted} / ~{TskModel.DurationEstimate}" :
+                    TskModel.DurationCompleted.HasValue ? TskModel.DurationCompleted.ToString() :
+                    TskModel.DurationEstimate.HasValue ? $"~{TskModel.DurationEstimate}" : "";
+
+    public string DueDateDisplay =>
+        (TskModel.DueDate.HasValue && !TskModel.CompletedDate.HasValue) ? TskModel.DueDate.Value.ToString("MM/dd/yyyy") : string.Empty;
+}
