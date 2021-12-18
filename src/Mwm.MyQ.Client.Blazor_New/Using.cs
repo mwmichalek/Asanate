68c783d1fd318891160a1edce18b8aacbeb14c5c
Michalek <mwmichalek@gmail.com>
2021-12-18 12:16:05 -0500

    Removed a bunch of crap

--- src/Mwm.MyQ.Client.Blazor_New/Using.cs
+++
@@ -1,6 +0,0 @@
-﻿global using Microsoft.AspNetCore.Components.Web;
-global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-global using Mwm.MyQ.Client.Blazor;
-global using Mwm.MyQ.Client.Service.Utils;
-global using Mwm.MyQ.Common.Utils;
-global using Syncfusion.Blazor;
