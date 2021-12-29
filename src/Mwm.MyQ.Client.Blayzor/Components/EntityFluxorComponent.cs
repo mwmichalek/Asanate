e42e797485d592a8956d3bde754f1d43bd1e15e7
mwmichalek <mwmichalek@gmail.com>
2021-12-28 19:30:47 -0500

    Started EntityFluxorComponent base.

---
+++ src/Mwm.MyQ.Client.Blayzor/Components/EntityFluxorComponent.cs
@@ -0,0 +1,56 @@
+﻿using Fluxor;
+using Fluxor.Blazor.Web.Components;
+using Microsoft.AspNetCore.Components;
+using Mwm.MyQ.Client.Service.Facades;
+using Mwm.MyQ.Client.Service.Store.Features.Shared.Helpers;
+using Mwm.MyQ.Client.Service.Store.State.Shared;
+using Mwm.MyQ.Domain;
+
+namespace Mwm.MyQ.Client.Blayzor.Components;
+
+public abstract class EntityFluxorComponent : FluxorComponent {
+
+    [Inject]
+    public IState<EntityState<Tsk>> TsksState { get; set; }
+
+    [Inject]
+    public IState<EntityState<Initiative>> InitiativesState { get; set; }
+
+    [Inject]
+    public IState<EntityState<Project>> ProjectsState { get; set; }
+
+    [Inject]
+    public IState<EntityState<Company>> CompaniesState { get; set; }
+
+    [Inject]
+    public EntityStateFacade EntityStateFacade { get; set; }
+
+    public bool IsLoading() => TsksState.IsLoading() ||
+                           InitiativesState.IsLoading() ||
+                           ProjectsState.IsLoading() ||
+                           CompaniesState.IsLoading();
+
+    public bool HasErrors() => TsksState.HasErrors() ||
+                               InitiativesState.HasErrors() ||
+                               ProjectsState.HasErrors() ||
+                               CompaniesState.HasErrors();
+
+    public bool HasValues() => TsksState.HasValue(true) &&
+                               InitiativesState.HasValue(true) &&
+                               ProjectsState.HasValue(true) &&
+                               CompaniesState.HasValue(true);
+
+    protected override void OnInitialized() {
+        if (!TsksState.HasValue())
+            EntityStateFacade.Load<Tsk>();
+        if (!InitiativesState.HasValue())
+            EntityStateFacade.Load<Initiative>();
+        if (!CompaniesState.HasValue())
+            EntityStateFacade.Load<Company>();
+        if (!ProjectsState.HasValue())
+            EntityStateFacade.Load<Project>();
+
+        base.OnInitialized();
+    }
+
+}
--- src/Mwm.MyQ.Client.Blayzor/Components/EntityGrid.razor.cs
+++ src/Mwm.MyQ.Client.Blayzor/Components/EntityGrid.razor.cs
@@ -1,6 +1,21 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Fluxor;
+using Fluxor.Blazor.Web.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using Mwm.MyQ.Client.Service.Facades;
+using Mwm.MyQ.Client.Service.Store.State.Shared;
+using Mwm.MyQ.Domain;
 
 namespace Mwm.MyQ.Client.Blayzor.Components;
 
-public partial class EntityGrid : ComponentBase {
+public partial class EntityGrid : EntityFluxorComponent {
+
+    [Inject]
+    ILogger<EntityGrid> Logger { get; set; }
+
+    protected override void OnInitialized() {
+        base.OnInitialized(); 
+
+    }
+
 }
