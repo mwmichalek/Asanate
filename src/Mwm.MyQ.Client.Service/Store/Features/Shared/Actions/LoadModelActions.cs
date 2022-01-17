bbdb9cd8ae2ab33807121714ca1b5f4c3bdd3cfe
Michalek <mwmichalek@gmail.com>
2022-01-17 12:57:25 -0500

    Slowly building out model pipeline

---
+++ src/Mwm.MyQ.Client.Service/Store/Features/Shared/Actions/LoadModelActions.cs
@@ -0,0 +1,26 @@
+﻿using Mwm.MyQ.Client.Service.Models;
+using Mwm.MyQ.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mwm.MyQ.Client.Service.Store.Features.Shared.Actions {
+    public class LoadModelAction<TEntity> where TEntity : INamedEntity {
+    }
+
+    public class LoadModelSuccessAction<TEntity> where TEntity : INamedEntity {
+
+        public LoadModelSuccessAction(IEnumerable<EntityModel<TEntity>> models) =>
+            Models = models;
+
+        public IEnumerable<EntityModel<TEntity>> Models { get; }
+
+    }
+
+    public class LoadModelFailureAction<TEntity> : FailureAction where TEntity : INamedEntity {
+
+        public LoadModelFailureAction(string errorMessage) : base(errorMessage) { }
+    }
+
+}
---
+++ src/Mwm.MyQ.Client.Service/Store/Features/Shared/Effects/LoadModelEffect.cs
@@ -0,0 +1,39 @@
+﻿using Fluxor;
+using Microsoft.Extensions.Logging;
+using Mwm.MyQ.Client.Service.Models;
+using Mwm.MyQ.Client.Service.Storage;
+using Mwm.MyQ.Client.Service.Store.Features.Shared.Actions;
+using Mwm.MyQ.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Mwm.MyQ.Client.Service.Store.Features.Shared.Effects {
+    public abstract class LoadModelEffect<TEntity> : Effect<LoadModelAction<TEntity>> where TEntity : INamedEntity {
+
+        protected readonly ILogger<LoadEntityEffect<TEntity>> _logger;
+        
+        public LoadModelEffect(ILogger<LoadEntityEffect<TEntity>> logger) =>
+            (_logger) = (logger);
+
+        public override async Task HandleAsync(LoadModelAction<TEntity> action, IDispatcher dispatcher) {
+            var entityName = typeof(TEntity).Name;
+            try {
+                _logger.LogInformation($"Loading models {entityName}(s) ...");
+
+                //TODO: (MWM) Create Models here.
+                var models = new List<EntityModel<TEntity>>();
+
+                _logger.LogInformation($"Loaded models {models.Count} {entityName}(s) successfully!");
+                dispatcher.Dispatch(new LoadModelSuccessAction<TEntity>(models));
+            } catch (Exception e) {
+                _logger.LogError($"Error loading {entityName}(s), reason: {e}");
+                dispatcher.Dispatch(new LoadEntityFailureAction<TEntity>(e.Message));
+            }
+
+        }
+    }
+}
