79d558ba5c5da90325b3c3e0701e41240bf8345b
mwmichalek <mwmichalek@gmail.com>
2022-01-19 20:48:00 -0500

    Test me

---
+++ src/Mwm.MyQ.Client.Service/Store/Features/TskModels/TskModelReducers.cs
@@ -0,0 +1,30 @@
+﻿using Fluxor;
+using Mwm.MyQ.Client.Service.Store.Features.Shared.Actions;
+using Mwm.MyQ.Client.Service.Store.State.Shared;
+using Mwm.MyQ.Domain;
+using Mwm.MyQ.Application.Tsks.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mwm.MyQ.Client.Service.Store.Features.Tsks {
+    public static class TskModelReducers {
+        [ReducerMethod]
+        public static ModelState<Tsk> ReduceLoadTsksAction(ModelState<Tsk> state, LoadEntityAction<Tsk> _) =>
+            new ModelState<Tsk>(true, null, null, state.CurrentModel);
+
+        [ReducerMethod]
+        public static ModelState<Tsk> ReduceLoadTsksSuccessAction(ModelState<Tsk> state, LoadEntitySuccessAction<Tsk> action) =>
+            new ModelState<Tsk>(false, null, action.Entities, state.CurrentModel);
+
+        [ReducerMethod]
+        public static ModelState<Tsk> ReduceLoadTsksFailureAction(ModelState<Tsk> state, LoadEntityFailureAction<Tsk> action) =>
+            new ModelState<Tsk>(false, action.ErrorMessage, null, state.CurrentModel);
+
+
+
+    }
+}
+
+
