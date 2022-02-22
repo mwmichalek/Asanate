df86c772af2e36401762280bd89c4d49f24f97cc
mwmichalek <mwmichalek@gmail.com>
2022-02-21 20:14:48 -0500

    MyQ be loading

--- src/Mwm.MyQ.Client.Mud/Program.cs
+++ src/Mwm.MyQ.Client.Mud/Program.cs
@@ -2,13 +2,34 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Mwm.MyQ.Client.Mud;
 using MudBlazor.Services;
+using Mwm.MyQ.Client.Service.Utils;
+using Mwm.MyQ.Common.Utils;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//TEMPLATE: builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+var services = builder.Services;
+var myCloneApiUrl = builder.Configuration["MyClone:Api:Url"];
+Console.WriteLine($"Middle Tier: [{myCloneApiUrl}]");
 
-builder.Services.AddMudServices();
-await builder.Build().RunAsync();
+services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001") });
+//OLD: services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001") });
+
+var configuration = services.AddConfigurationWithUserSecrets();
+services.AddLogging();
+services.AddClientServices();
+
+services.AddMudServices();
+
+var server = builder.Build();
+var serviceProvider = server.Services;
+
+var logger = serviceProvider.GetService<ILogger<Program>>();
+logger?.LogInformation(">>> STARTING SERVICE <<<");
+await serviceProvider.UseClientServicesAsync();
+logger?.LogInformation(">>> LOADED DATA <<<");
+
+await server.RunAsync();
