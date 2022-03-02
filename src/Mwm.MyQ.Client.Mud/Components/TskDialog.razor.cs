f2e77a8bcb4a936fba397e3d0d5e51d2ff5551cf
mwmichalek <mwmichalek@gmail.com>
2022-03-01 21:22:47 -0500

    Have dialog appearing but needs love.

--- src/Mwm.MyQ.Client.Mud/Components/TskDialog.razor.cs
+++ src/Mwm.MyQ.Client.Mud/Components/TskDialog.razor.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Fluxor;
+using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Logging;
 using Mwm.MyQ.Client.Service.Facades;
 using Mwm.MyQ.Client.Service.Models;
+using Mwm.MyQ.Client.Service.Store.State.Shared;
+using Mwm.MyQ.Domain;
+using System.Threading.Tasks;
 
 namespace Mwm.MyQ.Client.Mud.Components;
 
@@ -13,11 +17,23 @@ public partial class TskDialog: ComponentBase {
     [Inject]
     public EntityStateFacade EntityStateFacade { get; set; }
 
+    [Inject]
+    public IState<ModelState<TskModel, Tsk>> ModelsState { get; set; }
+
     public bool IsDialogShowing { get; set; }
 
     public bool IsNew => TskModel != null && TskModel.Id == 0;
 
     public TskModel TskModel;
+
+    protected override async Task OnInitializedAsync() {
+        ModelsState.StateChanged += async (s, e) => {
+            if (e.CurrentModel != null)
+                Update(e.CurrentModel);
+            else
+                Close();
+        };
+    }
 
     public void Add(int? initiativeId) {
         Logger.LogInformation($"Adding TskModel.");
