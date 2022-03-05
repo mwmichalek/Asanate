8910143bf9bfa2ac82dee375924051e6c5dc37ac
Michalek <mwmichalek@gmail.com>
2022-03-05 14:20:19 -0500

    Dialog is looking good.

--- src/Mwm.MyQ.Client.Mud/Components/TskDialog.razor.cs
+++ src/Mwm.MyQ.Client.Mud/Components/TskDialog.razor.cs
@@ -2,10 +2,12 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Logging;
 using MudBlazor;
+using Mwm.MyQ.Application.Tsks.Commands;
 using Mwm.MyQ.Client.Service.Facades;
 using Mwm.MyQ.Client.Service.Models;
 using Mwm.MyQ.Client.Service.Store.State.Shared;
 using Mwm.MyQ.Domain;
+using System;
 using System.Threading.Tasks;
 
 namespace Mwm.MyQ.Client.Mud.Components;
@@ -56,11 +58,44 @@ public partial class TskDialog: ComponentBase {
         StateHasChanged();
     }
 
-    public void Save() {
-        Logger.LogInformation($"Saving TskModel.");
-        IsDialogShowing = false;
-        TskModel = null;
-        StateHasChanged();
+
+    public async Task SaveAsync() {
+        Logger.LogInformation($"Saving Dialog.");
+        try {
+            if (TskModel.Id == 0) {
+                Logger.LogInformation($"Add: {TskModel.Name}");
+                await EntityStateFacade.Add<Tsk, TskAdd.Command>(new TskAdd.Command {
+                    Name = TskModel.Name,
+                    Status = TskModel.Status,
+                    DurationEstimate = TskModel.DurationEstimate,
+                    DurationCompleted = TskModel.DurationCompleted,
+                    Notes = TskModel.Notes,
+                    DueDate = TskModel.DueDate,
+                    StartDate = TskModel.StartDate,
+                    StartedDate = TskModel.StartedDate,
+                    CompletedDate = TskModel.CompletedDate
+                });
+
+            } else {
+                Logger.LogInformation($"Update: {TskModel.Name}");
+                await EntityStateFacade.Update<Tsk, TskUpdate.Command>(new TskUpdate.Command {
+                    Id = TskModel.Id,
+                    Name = TskModel.Name,
+                    Status = TskModel.Status,
+                    DurationEstimate = TskModel.DurationEstimate,
+                    DurationCompleted = TskModel.DurationCompleted,
+                    Notes = TskModel.Notes,
+                    DueDate = TskModel.DueDate,
+                    StartDate = TskModel.StartDate,
+                    StartedDate = TskModel.StartedDate,
+                    CompletedDate = TskModel.CompletedDate
+                });
+                
+            }
+            Close();
+        } catch (Exception ex) {
+            Logger.LogError($"Unable to update: {TskModel.Name}, {ex}");
+        }
     }
 
     public void Close() {
