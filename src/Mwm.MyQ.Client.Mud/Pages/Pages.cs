062a34560616219ed4153e8dfa1b4d31b82d1182
mwmichalek <mwmichalek@gmail.com>
2022-03-11 11:55:52 -0500

    Added navigation, will be able to dynamically create the top nav.

---
+++ src/Mwm.MyQ.Client.Mud/Pages/Pages.cs
@@ -0,0 +1,7 @@
+﻿namespace Mwm.MyQ.Client.Mud.Pages;
+
+public enum Page {
+
+    Kanban,
+    Notes
+}
---
+++ src/Mwm.MyQ.Client.Mud/Shared/TopNav.razor.cs
@@ -0,0 +1,37 @@
+﻿using Fluxor.Blazor.Web.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Mwm.MyQ.Client.Mud.Pages;
+using System;
+using System.Threading.Tasks;
+
+namespace Mwm.MyQ.Client.Mud.Shared;
+
+public partial class TopNav {
+
+    [Inject]
+    NavigationManager NavigationManager { get; set; }
+
+    public Page CurrentPage { get; set; } = Page.Kanban;
+
+    protected override Task OnInitializedAsync() {
+        NavigationManager.LocationChanged += LocationChanged;
+        return base.OnInitializedAsync();
+    }
+
+    void LocationChanged(object sender, LocationChangedEventArgs e) {
+        Page newPage = Page.Kanban; // Default
+        foreach (var pageName in Enum.GetNames(typeof(Page))) {
+            if (e.Location.ToLower().Contains(pageName.ToLower())) {
+                newPage = (Page)Enum.Parse(typeof(Page), pageName);
+                break;
+            }
+        }
+
+        if (CurrentPage != newPage) {
+            CurrentPage = newPage;
+            StateHasChanged();
+        }
+    }
+
+}
