ea7000a73df194fe9f7b8621e677c28a8c94e5dc
Michalek <mwmichalek@gmail.com>
2022-03-12 14:31:07 -0500

    Initial dropdowns working, ways to go.

--- src/Mwm.MyQ.Client.Mud/Components/TskQuickPane.razor.cs
+++ src/Mwm.MyQ.Client.Mud/Components/TskQuickPane.razor.cs
@@ -37,47 +37,51 @@ public partial class TskQuickPane : FluxorComponent {
     [Inject]
     public EntityStateFacade EntityStateFacade { get; set; }
 
-    public string NewTskName { get; set; } = string.Empty;
+    //public string NewTskName { get; set; } = string.Empty;
 
-    public string PendingTskName { get; set; } = string.Empty;
+    //public string PendingTskName { get; set; } = string.Empty;
 
-    public string NewTskEstimatedDuration { get; set; }
+    //public string NewTskEstimatedDuration { get; set; }
 
-    public string NewTskStatus { get; set; } = Status.Open.ToStr();
+    //public string NewTskStatus { get; set; } = Status.Open.ToStr();
 
-    public string NewInitiativeName { get; set; } = string.Empty;
+    //public string NewInitiativeName { get; set; } = string.Empty;
 
-    public string PendingInitiativeName { get; set; } = string.Empty;
+    //public string PendingInitiativeName { get; set; } = string.Empty;
 
-    public string NewInitiativeExternalId { get; set; } = string.Empty;
+    //public string NewInitiativeExternalId { get; set; } = string.Empty;
 
-    public string NewInitiativeExternalIdPrefix { get; set; } = string.Empty;
+    
 
-    public List<TskModel> SavedTskModels { get; set; } = new List<TskModel>();
+    //public List<TskModel> SavedTskModels { get; set; } = new List<TskModel>();
 
     public bool IsInInitiativeCreationMode { get; set; } = false;
 
-    public IEnumerable<DropDown<Project>> ProjectDropDowns { get; set; } = new List<DropDown<Project>>();
+    public string NewInitiativeExternalIdPrefix { get; set; } = string.Empty;
 
-    private DropDown<Project> selectedProjectDropDown;
 
-    public List<DropDown<Project>> SelectedProjectDropDown {
-        get { return new List<DropDown<Project>> { selectedProjectDropDown }; }
+    public List<Project> Projects { get; set; } = new List<Project>();
+
+    private Project selectedProject;
+
+    public Project SelectedProject {
+        get => selectedProject; 
         set {
-            selectedProjectDropDown = value.First();
+            selectedProject = value;
             UpdateInitiativeDropDown();
         }
     }
 
-    private int selectedInitiativeId = 0;
-    public int SelectedInitiativeId {
-        get { return selectedInitiativeId; }
+    public List<Initiative> Initiatives { get; set; } = new List<Initiative>();
+
+    private Initiative selectedInitiative;
+
+    public Initiative SelectedInitiative {
+        get => selectedInitiative; 
         set {
-            selectedInitiativeId = value;
+            selectedInitiative = value;
         }
     }
-
-    public IEnumerable<DropDown<Initiative>> InitiativeDropDowns { get; set; } = new List<DropDown<Initiative>>();
 
     protected override async Task OnInitializedAsync() {
         await base.OnInitializedAsync();
@@ -99,15 +103,13 @@ public partial class TskQuickPane : FluxorComponent {
 
     private void UpdateProjectDropDown() {
         if (ProjectsState.HasValue()) {
-            ProjectDropDowns = ProjectsState.Value.Entities.OrderBy(p => p.Name)
-                                                   .Select(p => new DropDown<Project>(p))
-                                                   .ToList();
+            Projects = ProjectsState.Value.Entities.OrderBy(p => p.Name).ToList();
 
             // If Project hasn't been set yet, then set it to 'Generic' 
-            if (selectedProjectDropDown == null) {
+            if (selectedProject == null) {
                 var genericProject = ProjectsState.Value.Entities.SingleOrDefault(p => p.Name == Project.DefaultProjectName);
-                if (genericProject != null)
-                    selectedProjectDropDown = new DropDown<Project>(genericProject);
+                if (genericProject != null) 
+                    selectedProject = genericProject;
             }
 
             UpdateInitiativeDropDown();
@@ -118,37 +120,31 @@ public partial class TskQuickPane : FluxorComponent {
 
         if (InitiativesState.HasValue()) {
             Logger.LogInformation("Updating Initiatives Dropdown.");
-            var selectedProjectId = selectedProjectDropDown.Entity.Id;
+            var selectedProjectId = selectedProject.Id;
             Project project = ProjectsState.FindById(selectedProjectId);
             NewInitiativeExternalIdPrefix = (project != null && !string.IsNullOrEmpty(project.ExternalIdPrexfix)) ?
                 $"{project.ExternalIdPrexfix}-" :
                 string.Empty;
 
-            var index = 1;
-            InitiativeDropDowns = InitiativesState.Value.Entities.Where(i => i.ProjectId == selectedProjectId)
-                                                                 .OrderBy(i => i.Name)
-                                                                 .Select(i => new DropDown<Initiative>(i))
-                                                                 .ToList();
-            var selectInitiativeName = (!string.IsNullOrEmpty(PendingInitiativeName)) ?
-                PendingInitiativeName : Initiative.DefaultInitiativeName;
-
-            // Whenever you change projects default to Triage
-            var selectInitiative = InitiativesState.Value.Entities.SingleOrDefault(i => i.ProjectId == selectedProjectId &&
-                                                                                        i.Name == selectInitiativeName);
-            if (selectInitiative != null) {
-                Logger.LogInformation($"Setting default Initiative to {selectInitiative.Name}");
-                selectedInitiativeId = selectInitiative.Id;
+            Initiatives = InitiativesState.Value.Entities.Where(i => i.ProjectId == selectedProjectId)
+                                                         .OrderBy(i => i.Name)
+                                                         .ToList();
+            // If initiative is not set or from a different project, update that shit
+            if (selectedInitiative == null || selectedInitiative.ProjectId != selectedProjectId) {
+                selectedInitiative = InitiativesState.Value.Entities.SingleOrDefault(i => i.ProjectId == selectedProjectId &&
+                                                                                          i.Name == Initiative.DefaultInitiativeName);
             }
 
+            StateHasChanged();
         }
     }
 
     public async Task SetInitiativeCreationMode(bool isInInitiativeCreationMode) {
         this.IsInInitiativeCreationMode = isInInitiativeCreationMode;
-        if (!isInInitiativeCreationMode) {
-            NewInitiativeName = string.Empty;
-            NewInitiativeExternalId = string.Empty;
-        }
+        //if (!isInInitiativeCreationMode) {
+        //    NewInitiativeName = string.Empty;
+        //    NewInitiativeExternalId = string.Empty;
+        //}
 
         StateHasChanged();
 
@@ -160,65 +156,65 @@ public partial class TskQuickPane : FluxorComponent {
     }
 
     public async Task Saving() {
-        try {
+        //try {
 
-            if (!string.IsNullOrEmpty(NewTskName) && !IsInInitiativeCreationMode) {
-                Logger.LogInformation($"Pending item: {NewTskName}");
-                float.TryParse(NewTskEstimatedDuration, out float estimatedDuration);
-                var tskStatus = NewTskStatus.ToStatus();
+        //    if (!string.IsNullOrEmpty(NewTskName) && !IsInInitiativeCreationMode) {
+        //        Logger.LogInformation($"Pending item: {NewTskName}");
+        //        float.TryParse(NewTskEstimatedDuration, out float estimatedDuration);
+        //        var tskStatus = NewTskStatus.ToStatus();
 
-                await EntityStateFacade.Add<Tsk, TskAdd.Command>(new TskAdd.Command {
-                    Name = NewTskName,
-                    DurationEstimate = estimatedDuration > 0 ? estimatedDuration : null,
-                    Status = tskStatus,
-                    InitiativeId = selectedInitiativeId
-                });
-                PendingTskName = NewTskName;
-                NewTskName = string.Empty;
-                NewTskEstimatedDuration = string.Empty;
-                StateHasChanged();
-                //await refTskName.FocusAsync();
-            } else if (!string.IsNullOrEmpty(NewInitiativeName) && IsInInitiativeCreationMode) {
-                Logger.LogInformation($"Pending item: {NewInitiativeName}");
+        //        await EntityStateFacade.Add<Tsk, TskAdd.Command>(new TskAdd.Command {
+        //            Name = NewTskName,
+        //            DurationEstimate = estimatedDuration > 0 ? estimatedDuration : null,
+        //            Status = tskStatus,
+        //            InitiativeId = selectedInitiative.Id
+        //        });
+        //        PendingTskName = NewTskName;
+        //        NewTskName = string.Empty;
+        //        NewTskEstimatedDuration = string.Empty;
+        //        StateHasChanged();
+        //        //await refTskName.FocusAsync();
+        //    } else if (!string.IsNullOrEmpty(NewInitiativeName) && IsInInitiativeCreationMode) {
+        //        Logger.LogInformation($"Pending item: {NewInitiativeName}");
 
-                await EntityStateFacade.Add<Initiative, InitiativeAdd.Command>(new InitiativeAdd.Command {
-                    Name = NewInitiativeName,
-                    ExternalId = NewInitiativeExternalId,
-                    ProjectId = selectedInitiativeId
-                });
-                PendingInitiativeName = NewInitiativeName;
-                NewInitiativeName = string.Empty;
-                NewInitiativeExternalId = string.Empty;
-                IsInInitiativeCreationMode = false;
-                StateHasChanged();
-                //await refTskName.FocusAsync();
-            }
-        } catch (Exception ex) {
-            Logger.LogError($"Unable to update: {NewTskName}, {ex}");
-        }
+        //        await EntityStateFacade.Add<Initiative, InitiativeAdd.Command>(new InitiativeAdd.Command {
+        //            Name = NewInitiativeName,
+        //            ExternalId = NewInitiativeExternalId,
+        //            ProjectId = selectedInitiative.Id
+        //        });
+        //        PendingInitiativeName = NewInitiativeName;
+        //        NewInitiativeName = string.Empty;
+        //        NewInitiativeExternalId = string.Empty;
+        //        IsInInitiativeCreationMode = false;
+        //        StateHasChanged();
+        //        //await refTskName.FocusAsync();
+        //    }
+        //} catch (Exception ex) {
+        //    Logger.LogError($"Unable to update: {NewTskName}, {ex}");
+        //}
     }
 
     public void Saved(EntityState<Tsk> entityState) {
-        if (entityState.CurrentEntity != null &&
-            entityState.CurrentEntity.Name == PendingTskName) {
-            Logger.LogInformation($"Adding item to list: {PendingTskName}");
+        //if (entityState.CurrentEntity != null &&
+        //    entityState.CurrentEntity.Name == PendingTskName) {
+        //    Logger.LogInformation($"Adding item to list: {PendingTskName}");
 
-            var tsk = entityState.CurrentEntity;
+        //    var tsk = entityState.CurrentEntity;
 
-            Initiative initiative = InitiativesState.FindById(tsk.InitiativeId);
-            Project project = (initiative != null) ? ProjectsState.FindById(initiative.ProjectId) : null;
-            Company company = (project != null) ? CompaniesState.FindById(project.CompanyId) : null;
+        //    Initiative initiative = InitiativesState.FindById(tsk.InitiativeId);
+        //    Project project = (initiative != null) ? ProjectsState.FindById(initiative.ProjectId) : null;
+        //    Company company = (project != null) ? CompaniesState.FindById(project.CompanyId) : null;
 
-            SavedTskModels.Insert(0, new TskModel {
-                Name = tsk.Name,
-                InitiativeName = initiative.Name,
-                ProjectName = project.Name,
-                CompanyName = company.Name,
-                DurationEstimate = tsk.DurationEstimate,
-                Status = tsk.Status
-            });
-            PendingTskName = string.Empty;
-        }
+        //    SavedTskModels.Insert(0, new TskModel {
+        //        Name = tsk.Name,
+        //        InitiativeName = initiative.Name,
+        //        ProjectName = project.Name,
+        //        CompanyName = company.Name,
+        //        DurationEstimate = tsk.DurationEstimate,
+        //        Status = tsk.Status
+        //    });
+        //    PendingTskName = string.Empty;
+        //}
     }
 
     //private void TskNameChanged(InputEventArgs args) => NewTskName = args.Value;
